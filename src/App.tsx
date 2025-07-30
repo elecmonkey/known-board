e74bc4ee11fe89ff671eea887bc211aecab03fc6
Elecmonkey <elecmonkey@hotmail.com>
2025-07-30 23:32:54 +0800

    build: use dynamic import to enable Vite bundle splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import type { Component } from 'solid-js';
+import { lazy } from 'solid-js';
 import { Router, Route } from '@solidjs/router';
 import { AppProvider } from './store';
 import { ToastProvider } from './components/Toast';
 import Layout from './components/Layout';
-import PendingPage from './pages/PendingPage';
-import CompletedPage from './pages/CompletedPage';
-import AllTasksPage from './pages/AllTasksPage';
-import AboutPage from './pages/AboutPage';
+
+const PendingPage = lazy(() => import('./pages/PendingPage'));
+const CompletedPage = lazy(() => import('./pages/CompletedPage'));
+const AllTasksPage = lazy(() => import('./pages/AllTasksPage'));
+const AboutPage = lazy(() => import('./pages/AboutPage'));
 
 const App: Component = () => {
   return (
