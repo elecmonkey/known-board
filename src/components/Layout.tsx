08394c6bb3ba0509902a6b5f575f1405113bea4d
Elecmonkey <elecmonkey@hotmail.com>
2025-07-31 13:44:02 +0800

    style: adjust layout style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout(props: LayoutProps) {
   return (
     <div class="min-h-screen flex flex-col">
       <Navigation />
-      <main class="flex-grow max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 w-full">
+      <main class="flex-grow max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 w-full">
         {props.children}
       </main>
       <Footer />
