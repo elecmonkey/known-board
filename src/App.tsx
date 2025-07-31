3d429337f602373c153306e3b7aa9214997655fb
Elecmonkey <elecmonkey@hotmail.com>
2025-08-01 00:50:47 +0800

    feat: prefetch other js bundle

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import type { Component } from 'solid-js';
-import { lazy } from 'solid-js';
+import { lazy, onMount } from 'solid-js';
 import { Router, Route } from '@solidjs/router';
 import { AppProvider } from './store';
 import { ToastProvider } from './components/Toast';
 import Layout from './components/Layout';
+import { preloadAllPages } from './routes-preload';
 
 const PendingPage = lazy(() => import('./pages/PendingPage'));
 const CompletedPage = lazy(() => import('./pages/CompletedPage'));
@@ -13,6 +14,13 @@ const AboutPage = lazy(() => import('./pages/AboutPage'));
 const GuidePage = lazy(() => import('./pages/GuidePage'));
 
 const App: Component = () => {
+  onMount(() => {
+    setTimeout(() => {
+      // 排除首屏页面 PendingPage.tsx
+      preloadAllPages('PendingPage.tsx');
+    }, 500);
+  });
+
   return (
     <AppProvider>
       <ToastProvider>
---
+++ src/routes-preload.ts
@@ -0,0 +1,18 @@
+// 收集所有页面组件
+const modules = import.meta.glob('./pages/*Page.tsx');
+
+console.log(modules);
+
+export function preloadAllPages(exceptFile: string = '') {
+  for (const path in modules) {
+    if (path.includes(exceptFile)) {
+      continue;
+    }
+    
+    modules[path]().then(() => {
+      console.log(`预加载: ${path}`);
+    }).catch((error) => {
+      console.warn(`预加载失败: ${path}`, error);
+    });
+  }
+}
