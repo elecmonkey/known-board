936c06d2622f9dd63ea34faa6563a4a5f0ab2b57
Elecmonkey <elecmonkey@hotmail.com>
2025-08-02 00:00:16 +0800

    type: add type file for solid-dnd

---
+++ src/types/solid-dnd.d.ts
@@ -0,0 +1,11 @@
+// TypeScript declarations for solid-dnd directives
+declare module "solid-js" {
+  namespace JSX {
+    interface Directives {
+      draggable: any;
+      droppable: any;
+    }
+  }
+}
+
+export {};
